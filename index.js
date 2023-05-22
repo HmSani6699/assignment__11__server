0a1dd925d3468b6d56af8d1816eebb50d67ce413
H.M Sadiq <hmsadikurrahman120@gmail.com>
2023-05-22 21:06:11 +0600

    load a single toy data

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion ,ObjectId} = require('mongodb');
 const port = process.env.PORT || 5000;
 
 
@@ -29,6 +29,28 @@ async function run() {
         await client.connect();
 
         const toyCarCollction = client.db("toyCarDB").collection('toyCar');
+
+        app.get('/addToy', async (req, res) => {
+            const result = await toyCarCollction.find().toArray();
+            res.send(result)
+        })
+
+        app.get('/toy', async (req, res) => {
+            console.log(req.query.email,'query');
+            let query = {};
+            if (req.query?.email) {
+              query = { email: req.query.email }
+            }
+            const result = await toyCarCollction.find(query).toArray();
+            res.send(result)
+          })
+
+        app.get('/addToy/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const result = await toyCarCollction.findOne(query);
+            res.send(result);
+          })
 
 
         app.post('/addToys', async (req, res) => {
