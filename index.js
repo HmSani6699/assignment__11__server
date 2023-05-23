1ceb740fa9069126f642606b7f4b2e987733b2b4
H.M Sadiq <hmsadikurrahman120@gmail.com>
2023-05-23 15:56:35 +0600

    added to delete method

--- index.js
+++ index.js
@@ -77,8 +77,14 @@ async function run() {
             res.send(result)
 
         })
-
-
+        
+        // Delete
+        app.delete('/addToy/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const result = await toyCarCollction.deleteOne(query);
+            res.send(result)
+        })
 
 
         // Send a ping to confirm a successful connection
