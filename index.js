1a15a4d3b49da8309b09054306e81bbc9ca5d565
H.M Sadiq <hmsadikurrahman120@gmail.com>
2023-05-23 17:46:30 +0600

    deploy to servir site

--- index.js
+++ index.js
@@ -26,7 +26,8 @@ const client = new MongoClient(uri, {
 async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
-        await client.connect();
+
+        // await client.connect();
 
         const toyCarCollction = client.db("toyCarDB").collection('toyCar');
 
@@ -88,7 +89,9 @@ async function run() {
 
 
         // Send a ping to confirm a successful connection
-        await client.db("admin").command({ ping: 1 });
+        
+        // await client.db("admin").command({ ping: 1 });
+
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
         // Ensures that the client will close when you finish/error
