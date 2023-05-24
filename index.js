5f8c364af76169f21faf6ef5b3ffd7aff6855532
H.M Sadiq <hmsadikurrahman120@gmail.com>
2023-05-24 16:56:42 +0600

    deploy to vercel

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.use(express.json());
 
 
 
-const uri = `mongodb+srv://${process.env.Db_user}:${process.env.Db_pass}@cluster0.fnxcgsn.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.fnxcgsn.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -96,7 +96,7 @@ async function run() {
 
         // Send a ping to confirm a successful connection
 
-        // await client.db("admin").command({ ping: 1 });
+        await client.db("admin").command({ ping: 1 });
 
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
